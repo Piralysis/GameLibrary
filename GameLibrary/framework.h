d9b983b09733d86fa22e0a4fb5e0000b5626a132
Piralysis <Piralysis@users.noreply.github.com>
2020-03-09 21:52:39 -0500

    Initial Commit. Basic window is created, but it does not do anything.

---
+++ GameLibrary/framework.h
@@ -0,0 +1,13 @@
+// framework.h : include file for standard system include files,
+// or project specific includes
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+// Windows Header Files
+#include <Windows.h>
+// Other includes
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
---
+++ GameLibrary/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by GameLibrary.rc
+//
+#define IDS_APP_TITLE                   101
+#define IDC_GAMELIBRARY                 102
+#define IDD_ABOUTBOX                    103
+#define IDI_GAMELIBRARY                 106
+#define IDI_SMALL                       107
+#define IDM_ABOUT                       40001
+#define ID_FILE_EXIT                    40003
+#define ID_HELP_                        40004
+#define IDM_EXIT                        40005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
